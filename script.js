619037c9c6e3c206d52b93424633da7feb8f76e0
LUIZA BITENCOURT VIEIRA <86606581087@senacrs.edu.br>
2026-08-13 15:13:07 -0300

    feat: button that 'runs away' from the cursor

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function CliqueAqui() {
     alert("Fofoqueiro");
 }
+
+const Botaoquefoge = document.getElementById('fujao')
+Botaoquefoge.addEventListener("mouseover", function () {
+    const largurajanela = window.innerWidth;
+    const alturajanela = window.innerHeight;
+    const largurabotao = Botaoquefoge.offsetWidth;
+    const alturabotao = Botaoquefoge.offsetHeight;
+    const novox = Math.floor(Math.random() * (largurajanela - largurabotao));
+    const novoy = Math.floor(Math.random() * (alturajanela - alturabotao));
+
+    Botaoquefoge.style.left = novox + "px";
+    Botaoquefoge.style.top = novoy + "px";
+})
